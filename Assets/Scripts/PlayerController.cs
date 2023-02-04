d5661c26b2e6d762a43bc03fff3f1e60b2644a16
Alp <elgunalp1@gmail.com>
2023-02-04 09:52:57 +0300

    Added player controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    [SerializeField] private float _forceToAdd;
+
+    private Rigidbody2D _rigitBody;
+
+    #region Life Cycle
+    private void Start()
     {
-        
+        _rigitBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            AddForce(_forceToAdd);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            AddForce(-_forceToAdd);
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "Stickable")
+            return;
+
+
+        HingeJoint2D addedJoint =  collision.gameObject.AddComponent<HingeJoint2D>();
+        addedJoint.connectedBody = _rigitBody;
+        addedJoint.enableCollision = true;
+        addedJoint.anchor = collision.contacts[0].point;
+        addedJoint.connectedAnchor = collision.contacts[0].point;
+    }
+    #endregion
+
+    #region Movement
+    private void AddForce(float force)
+    {
+        _rigitBody.AddTorque(force);
+    }
+    #endregion
 }
