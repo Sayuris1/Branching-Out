dd1a2f42a554beb4e4427d105a1bfcbb425277b7
Yusuf <yusuftaha9@gmail.com>
2023-02-05 13:54:42 +0300

    Change cooldown UI

--- Assets/Scripts/CooldownUI.cs
+++ Assets/Scripts/CooldownUI.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CooldownUI : MonoBehaviour
 {
-    [SerializeField] private RectTransform trans;
+    [SerializeField] private Image image;
 
     private void Update()
     {
-        float x = Mathf.Lerp(trans.localScale.x, PlayerController.Instance.GetCooldownPercent(),Time.deltaTime * 5);
-        trans.localScale = new Vector3(x, 1, 1);
+        float target = PlayerController.Instance.GetCooldownPercent();
+        image.fillAmount = Mathf.Lerp(image.fillAmount, target,Time.deltaTime * 12);
     }
 }
